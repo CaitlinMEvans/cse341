a6db6e314e38df093964b53801bb765c77a3383a
Caitlin Evans <caitmsevans@gmail.com>
2025-04-15 21:29:19 -0600

    render error

--- petTracker/server.js
+++ petTracker/server.js
@@ -127,10 +127,10 @@ app.use(cors({
 }));
 
 // Only start server if not in test environment
-if (process.env.NODE_ENV !== 'test') {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+// if (process.env.NODE_ENV !== 'test') {
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// }
 
 module.exports = app;
